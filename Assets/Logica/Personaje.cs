c31ccc2692da71163f9a122a68031ca7dcb64f5d
Jesus Aponte <jechuramirez1999@gmail.com>
2025-05-29 13:03:46 -0500

    Escenario, personaje Atahualpa con su movimiento y salto sin animaciones

---
+++ Assets/Logica/Personaje.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+
+public class Personaje : MonoBehaviour
+{
+    public float velocidad;
+    public float fuerzaSalto;
+    public int saltosMaximos;
+    public LayerMask capaSuelo;
+
+    private Rigidbody2D rigidbody2D;
+    private BoxCollider2D boxCollider;
+    private bool mirandoDerecha = true;
+    private int saltosRestantes;
+    private bool estabaEnSuelo;
+
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
+        saltosRestantes = saltosMaximos;
+    }
+
+    void Update()
+    {
+        ProcesarMovimiento();
+        ProcesarSalto();
+    }
+
+    bool EstaEnSuelo()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(
+            boxCollider.bounds.center,
+            boxCollider.bounds.size,
+            0f,
+            Vector2.down,
+            0.2f,
+            capaSuelo
+        );
+        return raycastHit.collider != null;
+    }
+
+    void ProcesarSalto()
+    {
+        bool enSuelo = EstaEnSuelo();
+        if (enSuelo && !estabaEnSuelo)
+        {
+            saltosRestantes = saltosMaximos;
+        }
+        estabaEnSuelo = enSuelo;
+
+        if (Input.GetKeyDown(KeyCode.Space) && saltosRestantes > 0)
+        {
+            saltosRestantes--;
+            rigidbody2D.linearVelocity = new Vector2(rigidbody2D.linearVelocity.x, 0f); // Resetear velocidad vertical
+            rigidbody2D.AddForce(Vector2.up * fuerzaSalto, ForceMode2D.Impulse);
+        }
+    }
+
+    void ProcesarMovimiento()
+    {
+        float inputMovimiento = Input.GetAxis("Horizontal");
+        rigidbody2D.linearVelocity = new Vector2(inputMovimiento * velocidad, rigidbody2D.linearVelocity.y);
+        GestionarOrientacion(inputMovimiento);
+    }
+
+    void GestionarOrientacion(float inputMovimiento)
+    {
+        if ((mirandoDerecha && inputMovimiento < 0) || (!mirandoDerecha && inputMovimiento > 0))
+        {
+            mirandoDerecha = !mirandoDerecha;
+            transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+        }
+    }
+}
